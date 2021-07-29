e2a97819af0af51f338002259ebcefcc8dde46ab
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-07-29 21:00:12 +0530

    custom dropdown added

---
+++ src/modules/DropDown.jsx
@@ -0,0 +1,92 @@
+import {
+  React, useState, useRef,
+} from 'react';
+import useOutsideClick from './useOutsideClick';
+import '../index.scss';
+
+const options = [
+  { id: '5a15b13c36e7a7f00cf07cd', label: 'Karyn Wright' },
+  { label: 'Rochelle Russell', isdisabled: 'true' },
+  { id: '5a15b13c663ea0af9ad0dae8', label: 'Mendoza Ruiz' },
+  { id: '5a15b13cc9eeb36511d65acf', label: 'Rosales Russell' },
+  { label: 'Marquez Nolan', isdisabled: 'true' },
+  { id: '5a15b13ca51b0aaf8a99c05a', label: 'Franklin James' },
+  { id: '5a15b13cc3b9381ffcb1d6f7', label: 'Elsa Bradley' },
+  { id: '5a15b13ce58cb6ff62c65164', label: 'Pearson Thompson' },
+  { id: '5a15b13c90b95eb68010c86e', label: 'Ina Pugh' },
+  { id: '5a15b13c2b1746e12788711f', label: 'Nguyen Elliot' },
+  { id: '5a15b13c605403381eec5019', label: 'Mills Barnett' },
+  { id: '5a15b13c67e2e6d1a3cd6ca5', label: 'Margaret Reynolds' },
+  { id: '5a15b13c947c836d177aa85c', label: 'Yvette Navarro' },
+  { id: '5a15b13c5dbbe61245c1fb73', label: 'Elisa Guzman' },
+  { id: '5a15b13c38fd49fefea8db80', label: 'Jodie Bowman' },
+  { id: '5a15b13c9680913c470eb8fd', label: 'Diann Booker' },
+];
+
+export default function Dropdown() {
+  const [open, setOpen] = useState(false);
+  const [selection, setSelection] = useState(options[0]);
+  const toggle = () => setOpen(!open);
+  const ref = useRef();
+  useOutsideClick(ref, () => {
+    setOpen(false);
+  });
+  return (
+    <>
+      <div ref={ref} className="ddWrapper">
+        <div className="ddInputWrapper">
+          <div className="ddHeaderTitle">
+            <input
+              className="ddHeaderTitleBold"
+              value={selection.label}
+              onFocus={() => setOpen(true)}
+              onChange={(e) => setSelection({ label: e.target.value })}
+            />
+          </div>
+          <button className="ddClear" type="button" onClick={() => { setSelection({ label: '' }); }}>
+            ×
+          </button>
+          <span className="ddSpan"> | </span>
+          <div
+            tabIndex={0}
+            className="ddHeader"
+            role="button"
+            onKeyPress={() => toggle(!open)}
+            onClick={() => toggle(!open)}
+          >
+            <div className="ddHeaderAction">
+              <span className="ddClose">{open ? '▲' : '▼'}</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>
+            Selected:
+            {selection.id}
+          </p>
+        </div>
+        {open && (
+          <ul className="ddList">
+            {options.filter((item) => item.label.toLowerCase()
+              .indexOf(selection.label.toLowerCase()) !== -1)
+              .map((item) => (
+                <li className="ddListItem" key={item.id}>
+                  <button
+                    className="ddButton"
+                    type="button"
+                    onClick={() => {
+                      const selectedValue = item;
+                      setSelection(selectedValue);
+                    }}
+                    disabled={!!item.isdisabled}
+                  >
+                    <span>{item.label}</span>
+                  </button>
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
--- src/modules/bodyContainer.jsx
+++ src/modules/bodyContainer.jsx
@@ -1,26 +1,7 @@
 /* eslint-disable linebreak-style */
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React from 'react';
+import Dropdown from './DropDown';
 import '../index.scss';
-
-const options = [
-  { value: '5a15b13c36e7a7f00cf07cd', label: 'Karyn Wright' },
-  { label: 'Rochelle Russell', isdisabled: 'true' },
-  { value: '5a15b13c663ea0af9ad0dae8', label: 'Mendoza Ruiz' },
-  { value: '5a15b13cc9eeb36511d65acf', label: 'Rosales Russell' },
-  { label: 'Marquez Nolan', isdisabled: 'true' },
-  { value: '5a15b13ca51b0aaf8a99c05a', label: 'Franklin James' },
-  { value: '5a15b13cc3b9381ffcb1d6f7', label: 'Elsa Bradley' },
-  { value: '5a15b13ce58cb6ff62c65164', label: 'Pearson Thompson' },
-  { value: '5a15b13c90b95eb68010c86e', label: 'Ina Pugh' },
-  { value: '5a15b13c2b1746e12788711f', label: 'Nguyen Elliot' },
-  { value: '5a15b13c605403381eec5019', label: 'Mills Barnett' },
-  { value: '5a15b13c67e2e6d1a3cd6ca5', label: 'Margaret Reynolds' },
-  { value: '5a15b13c947c836d177aa85c', label: 'Yvette Navarro' },
-  { value: '5a15b13c5dbbe61245c1fb73', label: 'Elisa Guzman' },
-  { value: '5a15b13c38fd49fefea8db80', label: 'Jodie Bowman' },
-  { value: '5a15b13c9680913c470eb8fd', label: 'Diann Booker' },
-];
 
 function ContentBody() {
   return (
@@ -36,32 +17,11 @@ function ContentBody() {
   );
 }
 
-function DropDown() {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
-  return (
-    <div className="dropDown">
-      <Select
-        placeholder="Select item"
-        className="select-drop"
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
-        options={options}
-        isOptionDisabled={(option) => option.isdisabled}
-        isClearable
-      />
-      <p className="caption">
-        Selected:
-        {selectedOption ? selectedOption.value : ' '}
-      </p>
-    </div>
-  );
-}
-
 function Body() {
   return (
     <div className="bodyContainer">
       <ContentBody />
-      <DropDown />
+      <Dropdown />
     </div>
   );
 }
